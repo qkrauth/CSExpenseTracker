a28f922e41b6cd4d890735784232c944c570ddfb
Quinten Krauth <quintenkrauth19@gmail.com>
2025-10-17 16:34:36 -0400

    expense file logic with comments

--- Expense.cs
+++ Expense.cs
@@ -1,9 +1,17 @@
-using System:
+using System: // basically like an import, gives access to basic C# functionalities
 
-namespace ExpenseTrackerApp
+namespace ExpenseTrackerApp // groups classes together under one namespace
 {
     public class Expense 
     {
-        
+        public DateTime Date { get; set; } // getter/setter methods, property can be read and modified
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+
+        public override string ToString() // string representation of an expense
+        {
+            return $"{Date.ToShortDateString(), -12} | {Category, -10} | {Description, -20} | ${Amount, 8:F2}";
+        }
     }
 }
