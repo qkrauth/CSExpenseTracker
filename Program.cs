749c1ca1efb9bd1f14a0839d5dc86e05816ed63f
Quinten Krauth <quintenkrauth19@gmail.com>
2025-10-18 18:05:21 -0400

    app file

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System; // basic system utilities
+
+namespace ExpenseTrackerApp // same namespace as the other files
+{
+    class Program
+    {
+        static void Main(string[] args) // main entry point for the program
+        {
+            ExpenseTrackerApp app = new ExpenseTrackerApp(); // create the main app object
+            app.Run();
+        }
+    }
+}
